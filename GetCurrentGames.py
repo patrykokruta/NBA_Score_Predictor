ab8489ca065809faf197536f3da2f56d857e9b26
Jakub Caputa <jjakubcaputa@gmail.com>
2020-02-11 13:32:12 +0100

    add current games

--- GetCurrentGames.py
+++ GetCurrentGames.py
@@ -4,16 +4,36 @@ from nba_api.stats.endpoints._base import Endpoint
 from nba_api.stats.library.http import NBAStatsHTTP
 from nba_api.stats.library.parameters import DayOffset, GameDate, LeagueID
 from nba_api.stats.endpoints import ScoreboardV2
+from datetime import date
+import pandas as pd
+import numpy as np
 
+from pandas import DataFrame
+
+today = date.today()
+print(today)
 nba_teams = teams.get_teams()
-#league_id_nullable="00",
-gamefinder = leaguegamefinder.LeagueGameFinder(league_id_nullable="00")
-games = gamefinder.get_data_frames()[0]
-games.head()
-Currentgames = games[games.GAME_DATE.str[:] == '2020-02-04']
 
-#a = ScoreboardV2(game_date="2020-2-5",).last_meeting.data
-b = ScoreboardV2(game_date="2020-2-4",).last_meeting.data
-print(Currentgames)
+#print("Chose date")
+#datka = input()
 
-print(b)
+jutro = "2020-02-12"
+d = {'teamA' : [], 'teamB' :[]}
+#ff = DataFrame(columns=['teamA', 'teamB'])
+b = ScoreboardV2(game_date=jutro,).last_meeting.data
+ran=b['data'].__len__()
+dff = DataFrame(index=np.arange(0,ran),columns=['teamA', 'teamB'])
+
+for n in range(ran):
+    x = b['data'][n][6]
+    y = b['data'][n][11]
+    dff.iloc[n].teamA = y
+    dff.iloc[n].teamB = x
+
+
+#print(d)
+#print(a)
+#print(b)
+print(dff)
+#print(Currentgames)
+print("jej")
